dd1ac0b93373f86a795ef44db0f10252353fe355
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-11-03 21:31:31 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Authpage from "./pages/Authpage";
+
+function App() {
+  return (
+    <>
+      <Authpage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Authpage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Authpage = () => {
+  return <div>AUTH</div>;
+};
+
+export default Authpage;
